194c833d982cda7b475ae4b1ea6614b5be4098b9
xxxfrank <zhang.qi@pku.edu.cn>
2019-03-31 10:29:50 +0800

    success to blackattack

--- blackboxattack.py
+++ blackboxattack.py
@@ -5,7 +5,7 @@ import torchvision.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.autograd import Variable
-
+import json
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
@@ -117,41 +117,65 @@ def addinterference():
     num_tointerrupted = 0
     toattactimg = []
     time_bound = 7
-    for image, label in choosen[:10]:
+    
+    for image, label in  choosen:
         # fix the label
         orimage = image[:]
         orlabel = label[:]
         label = label.to(device)  # change the original label to a new false one
         label = (label.data + 1) % 10
         label = label.detach()
-        
+        img_mean = torch.mean(image)
+        image_var = torch.sum((image - img_mean) ** 2) / (image.shape[-1] * image.shape[-2])
+        img_std = (torch.sqrt(image_var) / 8).expand_as(image)
         time = 0
         while time<time_bound:  # do until the image was classified to the new false one
             time+=1
             # add interferrence
-            # constuct a markov transform probability and sample a x'
-                # compute the mean and standard and deviations
-            img_mean = torch.mean(image)
-            img_var = torch.sqrt(1/(28*28)*(image-img_mean)*(image-img_mean))
-            image_ = torch.normal(image, img_var)
+            image_ = torch.normal(image, img_std)
+            image_= torch.clamp(image_,min=0,max=1)
             # choose a u randomly between [0,1]
             u = torch.rand(1)
             # feed x' to my network,we got c(x')[argmax y'] that is the probabilty of y' (the label we persume)
             image_ = image_.to(device)
             output = model(image_)
-            norm_output = output-torch.min(output)
+            """norm_output = output-torch.min(output)
             norm_output=norm_output/torch.sum(norm_output)
-            pp = output[label].data
+            pp = norm_output[0][label]"""
+            predict = torch.argmax(output)
             # distance between image and image_
-            #dist = torch.sqrt(torch.sum((image-image_)*(image-image_)))
-            #print(dist)
-            # if u<c(x')[argmax y'] we reject x' and produce a new one;else we take x'
-            if u>=pp.cpu():
-                toattactimg.append((image_, label))
+            dist = torch.sum((orimage-image_)**2)/(image.shape[-1]*image.shape[-2])
+            delta_dist = image_var/4
+            if dist>delta_dist:
+                continue
+            if predict == label:
+                toattactimg.append((image_, label, orimage, orlabel))
                 num_tointerrupted+=1
                 break
+            image = image_[:]
     print(num_tointerrupted)
+    cnt = set()
+    for image, label,orimage,orlabel in toattactimg:
+        output=model(image)
+        predict = torch.argmax(output)
+        if predict==label:
+            cnt.add(label.item())
+    print cnt
+    printimg(toattactimg)
+    
+def printimg(img):
+    number = 0
+    for image, label, orimage,orlabel in img:
+        number += 1
+        image = image.cpu().squeeze(0)
+        img = to_pil_image(image[:])
+        img.convert('RGB')
+        img.save('./black_attack_picsave/att/attimg%d.jpg' %(number))
 
-
+        orimage = orimage.cpu().squeeze(0)
+        img = to_pil_image(orimage[:])
+        img.convert('RGB')
+        img.save('./black_attack_picsave/ori/orimg%d.jpg' % (number))
+        
 addinterference()
 # printimg()
---
+++ unbelive.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.autograd import Variable
+from network import ResNet, ResidualBlock
+
+from PIL import Image
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+model = torch.load('resnet.pkl')
+print("model is loaded")
+
+to_pil_image = transforms.ToPILImage()
+
+
+def image_loader(image_name):
+    image = Image.open(image_name)
+    image = transforms.ToTensor()(image)
+    return image.to(device)
+
+
+
+
+if __name__ == '__main__':
+    img1 = image_loader('./test/attimg13.jpg')
+    img1 = img1.view((1,1,28,28))
+    output1 = model(img1)
+    prd1 = torch.argmax(output1)
+    img2 = image_loader('./test/orimg13.jpg')
+    img2 = img2.view((1, 1, 28, 28))
+    output2 = model(img2)
+    prd2 = torch.argmax(output2)
+    print(prd1,prd2)
